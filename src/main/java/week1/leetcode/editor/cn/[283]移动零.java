251f241c7ee4e4f3e4a1bb9bf341280d27d3777a
saifuku <saifuku@163.com>
2021-03-13 00:23:10 +0800

    add: [283]移动零.java

---
+++ src/main/java/week1/leetcode/editor/cn/[283]移动零.java
@@ -0,0 +1,39 @@
+//给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。 
+//
+// 示例: 
+//
+// 输入: [0,1,0,3,12]
+//输出: [1,3,12,0,0] 
+//
+// 说明: 
+//
+// 
+// 必须在原数组上操作，不能拷贝额外的数组。 
+// 尽量减少操作次数。 
+// 
+// Related Topics 数组 双指针 
+// 👍 986 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void moveZeroes(int[] nums) {
+        // 遍历，遇到0记录位置，
+        // 遇到非0，与移动到上一个0位
+        //
+        if (nums == null) {
+            return;
+        }
+        int j = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i > j) {
+                    nums[j] = nums[i];
+                    nums[i] = 0;
+                }
+                j++;
+            }
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
