26063c856f667bacbf71d272700eb6c70420860f
saifuku <saifuku@163.com>
2021-03-12 16:28:56 +0800

    mod: Split into week

--- src/main/java/leetcode/editor/cn/week1/[146]LRU 缓存机制.java
+++ src/main/java/leetcode/editor/cn/week1/[146]LRU 缓存机制.java
@@ -1,11 +1,11 @@
-package leetcode.editor.cn;//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
+package leetcode.editor.cn.week1;//运用你所掌握的数据结构，设计和实现一个 LRU (最近最少使用) 缓存机制 。
 //
 // 
 // 
-// 实现 leetcode.editor.cn.LRUCache 类：
+// 实现 leetcode.editor.cn.week1.LRUCache 类：
 //
 // 
-// leetcode.editor.cn.LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
+// leetcode.editor.cn.week1.LRUCache(int capacity) 以正整数作为容量 capacity 初始化 LRU 缓存
 // int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。 
 // void put(int key, int value) 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上
 //限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。 
@@ -23,13 +23,13 @@ package leetcode.editor.cn;//运用你所掌握的数据结构，设计和实现
 //
 // 
 //输入
-//["leetcode.editor.cn.LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+//["leetcode.editor.cn.week1.LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
 //[[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
 //输出
 //[null, null, null, 1, null, -1, null, -1, 3, 4]
 //
 //解释
-//leetcode.editor.cn.LRUCache lRUCache = new leetcode.editor.cn.LRUCache(2);
+//leetcode.editor.cn.week1.LRUCache lRUCache = new leetcode.editor.cn.week1.LRUCache(2);
 //lRUCache.put(1, 1); // 缓存是 {1=1}
 //lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
 //lRUCache.get(1);    // 返回 1
@@ -136,8 +136,8 @@ class LRUCache {
 }
 
 /**
- * Your leetcode.editor.cn.LRUCache object will be instantiated and called as such:
- * leetcode.editor.cn.LRUCache obj = new leetcode.editor.cn.LRUCache(capacity);
+ * Your leetcode.editor.cn.week1.LRUCache object will be instantiated and called as such:
+ * leetcode.editor.cn.week1.LRUCache obj = new leetcode.editor.cn.week1.LRUCache(capacity);
  * int param_1 = obj.get(key);
  * obj.put(key,value);
  */
